33cc3e768c675b8d38f24e25f5c3138b7b6eafa2
Kyrylo Polezhaiev <kirushyk@gmail.com>
2016-10-28 16:53:14 +0300

    Replace with F24 build

--- update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-SRCDIR=/usr/i686-w64-mingw32/sys-root/mingw/
-
-#for f in bin/*; do echo $SRCDIR/$f; done
-for f in bin/*; do cp $SRCDIR/$f $f; done
-for f in lib/gstreamer-1.5/*; do cp $SRCDIR/$f $f; done
-for f in lib/kurento/modules/*; do cp $SRCDIR/$f $f; done
